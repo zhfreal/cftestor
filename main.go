dd9bd348729c3ea81dae4064c9f9f953b45b3d37
zhfreal <zhfreal@gmail.com>
2022-04-11 20:44:48 +0800

    fix version

--- main.go
+++ main.go
@@ -98,7 +98,7 @@ func print_version() {
 func init() {
 	var printVersion bool
 
-	version = "v1.4.0"
+	version = "v1.4.1"
 	var help = `
     cftestor ` + version + `
     测试Cloudflare IP的延迟和速度，获取最快的IP！
